291697f4e2513bb40588bf1b218ea289557b2d24
bgklein7 <bgklein@outlook.com>
2018-03-09 12:13:50 -0500

    first commit

---
+++ TravelReviewSite/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviewSite.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+
+        //Navigation Property
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ TravelReviewSite/Models/Review.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviewSite.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set;}
+        public string ReviewTitle { get; set; }
+        public string AuthorUserName { get; set; }
+        public string ContentPreview { get; set; }
+        [Display(Name ="Rating (out of 10)")]
+        public int Rating { get; set; }
+        [Display(Name ="Date Reviewed")]
+        [DisplayFormat(DataFormatString="{0:yyyy/MM/dd}")]
+        public DateTime ReviewDate { get; set; }
+        [Display(Name ="Date of Travel")]
+        [DisplayFormat(DataFormatString ="{0:yyyy/MM}")]
+        public DateTime TravelDate { get; set; }
+
+        //Foreign Keys and Navigation Properties
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
